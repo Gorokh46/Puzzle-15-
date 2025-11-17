76d1fba245446d6844e744af9fb73bb26db52a53
Anna <annagorokhovskaya1@gmail.com>
2025-11-17 18:44:05 +0500

    Удалены все файлы, кроме README.md

--- LaunchingTheGame.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-import tkinter.messagebox
-from gameLogic import create_solved_board, move_tile, is_solved, get_empty_pos
-from GraphicalInterface import Puzzle15App
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = Puzzle15App(root)
-    root.mainloop()
